e2ee7a1bb0635698e3b9c8db4df3e2fa1c17471d
knipferrc <knipferrc@gmail.com>
2017-10-27 08:22:51 -0400

    fix deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 const app = new Koa()
 
-app.use(serve(path.resolve(__dirname, '..', 'build')))
+app.use(serve(path.resolve(__dirname, './', 'build')))
 
 app.use(async ctx => {
   await send(ctx, 'build/index.html')

7de7a49bb55a18bad8400a85b627180593e6273b
knipferrc <knipferrc@gmail.com>
2017-10-27 07:40:46 -0400

    setup server

---
+++ server.js
@@ -0,0 +1,14 @@
+const Koa = require('koa')
+const serve = require('koa-static')
+const send = require('koa-send')
+const path = require('path')
+
+const app = new Koa()
+
+app.use(serve(path.resolve(__dirname, '..', 'build')))
+
+app.use(async ctx => {
+  await send(ctx, 'build/index.html')
+})
+
+app.listen(3000)

fa64cb5b0e459aa3e4241b0ac98a83f3503e3df4
knipferrc <knipferrc@gmail.com>
2017-10-30 07:43:04 -0400

    cleanup

--- src/components/Textarea/index.js
+++ src/components/Textarea/index.js
@@ -3,18 +3,32 @@ import styled from 'styled-components'
 
 const StyledTextArea = styled.textarea`
   background: #fff;
-  border: 0;
+  border: ${props => (props.hasError ? '1px solid red' : 0)};
   outline: 0;
   padding: 10px; 18px;
+  width: ${props => props.fullWidth && '100%'};
 `
 
-const Textarea = ({ placeholder, value, type, rows }) => {
+const Textarea = ({
+  type,
+  placeholder,
+  value,
+  rows,
+  name,
+  fullWidth,
+  onChange,
+  hasError
+}) => {
   return (
     <StyledTextArea
       type={type}
       placeholder={placeholder}
       value={value}
       rows={rows || '4'}
+      name={name}
+      fullWidth={fullWidth}
+      onChange={onChange}
+      hasError={hasError}
     />
   )
 }

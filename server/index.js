4b1a046ed3958edd0d1122cbe814d0873a50c50a
knipferrc <knipferrc@gmail.com>
2018-01-18 07:39:46 -0500

    move routes to own folder

--- server/index.js
+++ server/index.js
@@ -1,46 +1,28 @@
 const express = require('express')
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
-const { SubscriptionServer } = require('subscriptions-transport-ws')
-const { execute, subscribe } = require('graphql')
 const helmet = require('helmet')
 const Bundler = require('parcel-bundler')
-const compression = require('compression')
 const hpp = require('hpp')
+const compression = require('compression')
+
+const { SubscriptionServer } = require('subscriptions-transport-ws')
+const { execute, subscribe } = require('graphql')
 const { createServer } = require('http')
 
+const routes = require('./routes')
 const schema = require('./api')
-
-const PORT = 1234
 
 const bundler = new Bundler('index.html')
 const app = express()
 
+const PORT = 1234
+
 app.disable('x-powered-by')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
 app.use(helmet())
 app.use(compression())
 app.use(hpp())
-
-app.use(
-  '/graphql',
-  graphqlExpress(req => ({
-    schema,
-    context: {
-      db: 'db-test'
-    }
-  }))
-)
-
-app.use(
-  '/graphiql',
-  graphiqlExpress({
-    endpointURL: 'graphql',
-    subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
-  })
-)
-
+app.use(routes)
 app.use(bundler.middleware())
 
 const ws = createServer(app)

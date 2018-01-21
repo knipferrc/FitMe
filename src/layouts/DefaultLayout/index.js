c6297dc3d53c0dc84fd30047db30b25d4d7142f9
alexknipfer <alexanderknipfer@gmail.com>
2018-01-20 21:49:40 -0600

    Migrate to ant and create new layout

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,12 +1,71 @@
 import React from 'react'
 
-import NavBar from '../../components/NavBar'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd'
+const { SubMenu } = Menu
+const { Header, Content, Sider } = Layout
 
 const DefaultLayout = ({ children }) => (
-  <React.Fragment>
-    <NavBar />
-    {children}
-  </React.Fragment>
+  <Layout>
+    <Header className="header">
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['1']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">nav 1</Menu.Item>
+      </Menu>
+    </Header>
+    <Layout>
+      <Sider width={200} style={{ background: '#fff' }}>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100%', borderRight: 0 }}
+        >
+          <SubMenu
+            key="sub1"
+            title={
+              <span>
+                <Icon type="user" />subnav 1
+              </span>
+            }
+          >
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">option3</Menu.Item>
+            <Menu.Item key="4">option4</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            key="sub2"
+            title={
+              <span>
+                <Icon type="laptop" />subnav 2
+              </span>
+            }
+          >
+            <Menu.Item key="5">option5</Menu.Item>
+            <Menu.Item key="6">option6</Menu.Item>
+            <Menu.Item key="7">option7</Menu.Item>
+            <Menu.Item key="8">option8</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Sider>
+      <Layout style={{ height: '100%', padding: '0 24px 24px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>Login</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content
+          style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}
+        >
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
+  </Layout>
 )
 
 export default DefaultLayout
--- src/routes/Landing/LandingView/index.js
+++ src/routes/Landing/LandingView/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 import DefaultLayout from '../../../layouts/DefaultLayout'
+import LoginForm from '../LoginForm'
 
 const LandingView = () => (
   <DefaultLayout>
-    <h3>Landing View</h3>
+    <LoginForm />
   </DefaultLayout>
 )
 

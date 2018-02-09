3bbd4cea70259c8bfeee05830cb9e032d12de694
alexknipfer <alexanderknipfer@gmail.com>
2018-02-08 19:49:33 -0600

    Pass current user

--- src/routes/SharedRoutes/ChangePassword/index.js
+++ src/routes/SharedRoutes/ChangePassword/index.js
@@ -4,8 +4,12 @@ import DefaultLayout from 'layouts/DefaultLayout'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const ChangePassword = ({ history, location }) => (
-  <DefaultLayout history={history} location={location}>
+const ChangePassword = ({ history, location, currentUser }) => (
+  <DefaultLayout
+    history={history}
+    location={location}
+    currentUser={currentUser}
+  >
     <Card title="Change Password">Change Password</Card>
   </DefaultLayout>
 )

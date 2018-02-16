3ce2734aa1fc82e400309920742c33930aab2ec3
alexknipfer <alexanderknipfer@gmail.com>
2018-02-15 21:28:32 -0600

    Add trainers to cards with remove

--- src/routes/AdminRoutes/Dashboard/UserCard/index.js
+++ src/routes/AdminRoutes/Dashboard/UserCard/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Card, Icon, Avatar } from 'antd'
+import PropTypes from 'prop-types'
+
+import { Card, Icon, Tooltip } from 'antd'
 const { Meta } = Card
 
-const UserCard = () => {
+const UserCard = ({ _id, firstName, lastName, email, openRemoveUserModal }) => {
   return (
     <Card
-      style={{ width: 300 }}
+      style={{ width: 300, marginRight: 20 }}
       cover={
         <img
           alt="example"
@@ -15,18 +17,23 @@ const UserCard = () => {
       actions={[
         <Icon type="setting" />,
         <Icon type="edit" />,
-        <Icon type="ellipsis" />
+        <Tooltip title="Remove User" placement="bottom">
+          <Icon type="delete" onClick={() => openRemoveUserModal(_id)} />
+        </Tooltip>
       ]}
     >
-      <Meta
-        avatar={
-          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-        }
-        title="Card title"
-        description="This is the description"
-      />
+      <Meta title={`${firstName} ${lastName}`} description={email} />
     </Card>
   )
+}
+
+UserCard.propTypes = {
+  _id: PropTypes.string,
+  role: PropTypes.string,
+  email: PropTypes.string,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  openRemoveUserModal: PropTypes.func
 }
 
 export default UserCard
